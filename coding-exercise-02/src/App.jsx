316e505c5a90d27ea7d98fab9dbd8f9d9d1ccb76
Ian Baumeister <magnyt@gmail.com>
2025-04-03 00:01:08 +0200

    Extracted the Core Concept and created its own component

--- coding-exercise-02/src/App.jsx
+++ coding-exercise-02/src/App.jsx
@@ -1,6 +1,7 @@
-import reactImg from "./assets/react-core-concepts.png";
 import componentsImg from "./assets/components.png";
 import { CORE_CONCEPTS } from "./data.js";
+import Header from "./components/Header.jsx";
+import CoreConcept from "./components/CoreConcept.jsx";
 
 function CoreConcept(props) {
   return (
